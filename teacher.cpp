f8d74e29cc934fbc71f2cff449dbdf92bc115c33
Sergey Tolmachev <tolmachoff@gmail.com>
2022-02-25 10:14:44 +0300

    Counting time

--- teacher.cpp
+++ teacher.cpp
@@ -4,6 +4,7 @@
 #include <random>
 #include <iostream>
 #include <algorithm>
+#include <chrono>
 
 using namespace std;
 
@@ -30,6 +31,7 @@ void Teacher::add_lesson(const Lesson& lesson)
 
 void Teacher::teach(int repeats, bool to_shuffle)
 {
+    auto start = chrono::high_resolution_clock::now();
     for (int i = 0; i < repeats; ++i)
     {
         if (to_shuffle)
@@ -40,4 +42,10 @@ void Teacher::teach(int repeats, bool to_shuffle)
         for (const Lesson& lesson : d->lessons)
         d->neural.learn(lesson.x, lesson.y);
     }
+    auto finish = chrono::high_resolution_clock::now();
+    auto total_count = repeats * d->lessons.size();
+    auto total_ms = chrono::duration_cast<chrono::milliseconds>(finish - start).count();
+    auto per_one = static_cast<double>(chrono::duration_cast<chrono::microseconds>(finish - start).count()) / total_count;
+    cout << "Done " << total_count << " operations in " << total_ms << " ms" << endl;
+    cout << "Average operation time: " << per_one << " us" << endl;
 }
